b633ce9a9aff0c70389c7ef8af7e64a2810fc385
siva3609 <134902211+siva3609@users.noreply.github.com>
2023-05-29 10:27:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,19 +27,6 @@ module "securitygroup" {
   db_subnet_id   = module.networking.dbsubnet_id
 }
 
-module "compute" {
-  source         = "./modules/compute"
-  location = module.resourcegroup.location_id
-  resource_group = module.resourcegroup.resource_group_name
-  web_subnet_id = module.networking.websubnet_id
-  app_subnet_id = module.networking.appsubnet_id
-  web_host_name = var.web_host_name
-  web_username = var.web_username
-  web_os_password = var.web_os_password
-  app_host_name = var.app_host_name
-  app_username = var.app_username
-  app_os_password = var.app_os_password
-}
 
 module "database" {
   source = "./modules/database"
